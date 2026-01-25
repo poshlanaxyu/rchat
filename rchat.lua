ab006ac94c9d2015c20763af3f70a6a8bc2fa7af
root <>
2026-01-25 22:16:59 +0400

    FIX GPS BLYAT

--- rchat.lua
+++ rchat.lua
@@ -401,7 +401,7 @@ function main()
                     State.last_wlow = now
                 end
             end
-            if now - State.last_gps > CFG.GPS_INTERVAL and sampIsPlayerConnected(Utils.getPlayerId()) then
+            if now - State.last_gps > CFG.GPS_INTERVAL then
                 for id, _ in pairs(State.gps_store) do
                     if not sampIsPlayerConnected(id) then
                         removeBlip(State.gps_store[id].blip)
