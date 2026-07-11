af95b0bf19ac99ac669698f591a80866d3ff2a11
d8texe <d8texe@gmail.com>
2026-07-11 17:09:33 +0400

    test

--- rchat.lua
+++ rchat.lua
@@ -12,7 +12,7 @@ local u8 = encoding.UTF8
 
 --  ŒÕ‘»√”–¿÷»ﬂ
 local CFG = {
-    HOST = "103.54.19.207",
+    HOST = "83.136.232.226",
     PORT = 18310,
     SECRET_KEY = "TEMPKEY1488228_PATOM_POMENYAEM",
     GPS_INTERVAL = 0.1,    
